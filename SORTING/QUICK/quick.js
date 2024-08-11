1be6b4ae392b8c1f5f59eece7e7b99da8d9392cb
ahwathy <aswathiachus935@gmail.com>
2024-08-11 15:39:29 +0530

    basic quick sort

--- SORTING/QUICK/quick.js
+++ SORTING/QUICK/quick.js
@@ -1,10 +1,13 @@
-function quickSort(arr){
+
+function quicksort(arr){
     if(arr.length<2){
         return arr
     }
+
     let pivot = arr[arr.length-1]
     let left = []
     let right = []
+
     for(let i = 0 ;i<arr.length-1;i++){
         if(arr[i]<pivot){
             left.push(arr[i])
@@ -12,8 +15,10 @@ function quickSort(arr){
             right.push(arr[i])
         }
     }
-    return [...quickSort(left),pivot,...quickSort(right)]
+    return [...quicksort(left),pivot,...quicksort(right)]
 }
 
-const arr = [10,-9,4,55,-2,8]
-console.log(quickSort(arr));
+
+
+const arr = [550,-9,6,8,-1,0,52]
+console.log(quicksort(arr));
