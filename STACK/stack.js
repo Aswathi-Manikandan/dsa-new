e1528015987b1671c8704c4d21695e2f1888a6ea
ahwathy <aswathiachus935@gmail.com>
2024-08-11 20:42:17 +0530

    stack base

--- STACK/stack.js
+++ STACK/stack.js
@@ -1,50 +1,42 @@
-class Stack {
-    constructor() {
-        this.items = [];
+class Stack{
+    constructor(){
+        this.items = []
     }
 
-    // Add an element to the top of the stack
-    push(element) {
-        this.items.push(element);
+    push(element){
+        this.items.push(element)
     }
 
-    // Remove and return the top element of the stack
-    pop() {
-        if (this.isEmpty()) {
-            return "Underflow";
+    isEmpty(){
+        return this.items.length === 0
+    }
+
+    pop(){
+        if(this.isEmpty()){
+            return "underflow"
         }
-        return this.items.pop();
+        return this.items.pop()
     }
 
-    // Return the top element without removing it
+    print(){
+       
+        return this.items.join(' ')
+    }
+
     peek() {
         if (this.isEmpty()) {
             return "No elements in the stack";
         }
         return this.items[this.items.length - 1];
     }
-
-    // Check if the stack is empty
-    isEmpty() {
-        return this.items.length === 0;
-    }
-
-    // Return the size of the stack
-    size() {
-        return this.items.length;
-    }
-
-    // Print the elements in the stack
-    printStack() {
-        return this.items.join(' ');
-    }
 }
 
-// Example usage:
-const stack = new Stack();
-stack.push(10);
-stack.push(20);
-stack.push(30);
-console.log(stack.printStack()); // Output: 10 20 30
-console.log(stack.pop()); // Output: 30
-console.log(stack.peek()); // Output: 20
+const stack = new Stack()
+console.log(stack.pop());
+stack.push(10)
+stack.push(89)
+stack.push(25)
+console.log(stack.print());
+console.log(stack.peek());
+
+
