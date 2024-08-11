5615ef5a7f4d60f8aae0e453a344c21b86067a06
ahwathy <aswathiachus935@gmail.com>
2024-08-11 12:16:10 +0530

    basic insertion

--- SORTING/INSERTION/insertion.js
+++ SORTING/INSERTION/insertion.js
@@ -1,15 +1,16 @@
-function insertionSort(arr){
-    for(let i = 1; i<arr.length;i++){
-        let numberToInsert = arr[i]
+function insertion(arr){
+    
+    for(let i=1;i<arr.length;i++){
+        let numbertoinsert = arr[i]
         let j = i-1
-        while(j>=0&&arr[j]>numberToInsert){
+        while(j>=0 && arr[j]>numbertoinsert){
             arr[j+1] = arr[j]
             j=j-1
         }
-        arr[j+1] = numberToInsert
+        arr[j+1] = numbertoinsert
     }
 }
-
-const arr = [8,20,-2,4,-6]
-insertionSort(arr)
+const arr = [8,20,-2,-99,-6]
+insertion(arr);
 console.log(arr);
+
