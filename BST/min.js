4d25a911693e7bd0a5f24b9aca2b6ed39268288f
ahwathy <aswathiachus935@gmail.com>
2024-09-01 19:51:28 +0530

    minimum value found

--- BST/min.js
+++ BST/min.js
@@ -1,58 +1,58 @@
-class Node {
-    constructor(value) {
-        this.value = value;
-        this.left = null;
-        this.right = null;
+class Node{
+    constructor(value){
+        this.value = value
+        this.left = null
+        this.right = null
     }
 }
 
-class BinarySearchTree {
-    constructor() {
-        this.root = null;
+class BinarySearchTree{
+    constructor(){
+        this.root = null
     }
 
-    isEmpty() {
-        return this.root === null;
+    isEmpty(){
+        return this.root === null
     }
 
-    insert(value) {
-        const newNode = new Node(value);
-        if (this.isEmpty()) {
-            this.root = newNode;  
-        } else {
-            this.insertNode(this.root, newNode);
+    insert(value){
+        const newNode = new Node(value)
+        if(this.isEmpty()){
+            this.root = newNode
+        }else{
+                this.insertNode(this.root,newNode)
         }
     }
 
-    insertNode(root, newNode) {
-        if (newNode.value < root.value) {
-            if (root.left === null) {
-                root.left = newNode;
-            } else {
-                this.insertNode(root.left, newNode); 
-            }
-        } else {
-            if (root.right === null) {
-                root.right = newNode;
-            } else {
-                this.insertNode(root.right, newNode); 
+    insertNode(root,newNode){
+        if(newNode.value < root.value){
+            if(root.left === null){
+                root.left = newNode
+            }else{
+                this.insertNode(root.left,newNode)
             }
         }
+        else{
+            if(root.right === null){
+                root.right = newNode
+            }else{
+                this.insertNode(root.right,newNode)
+            }
+        }
     }
 
-    findMin(root = this.root) {
-        while (root.left !== null) {
-            root = root.left;
+    findMin(root = this.root){
+        while(root.left!==null){
+            root = root.left
         }
-        return root.value  
+        return root.value
     }
 }
 
-// Example usage
-const bin = new BinarySearchTree();
-bin.insert(50);
-bin.insert(52);
-bin.insert(10);
-bin.insert(45);
+const bin  = new BinarySearchTree()
+bin.insert(85)
+bin.insert(5)
+bin.insert(-6)
+bin.insert(52)
 
-console.log(bin.findMin()); // Output: 10
+console.log(bin.findMin());
